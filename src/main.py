01fe487c625e2f298927aa4a396450f9a7437f26
Paul <102642008+Gibsol@users.noreply.github.com>
2022-05-27 12:50:28 +0000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -9,14 +9,14 @@ memory = psutil.virtual_memory()
 memory_total = getattr(memory, 'total') * BYTE_TO_MEGABYTE
 memory_free = getattr(memory, 'free') * BYTE_TO_MEGABYTE
 memory_used = getattr(memory, 'used') 
-memory_limit = (memory_used / 100 * 20) * BYTE_TO_MEGABYTE
+memory_limit = (memory_used / 100 * 20) * BYTE_TO_MEGABYTE # 20% of the RAM
 
 #disk
 disk = psutil.disk_usage('/')
 disk_free = getattr(disk, 'total') * BYTE_TO_MEGABYTE
 disk_total = getattr(disk, 'free') * BYTE_TO_MEGABYTE
 disk_used = getattr(disk, 'used') 
-disk_limit = (disk_used / 100 * 20) * BYTE_TO_MEGABYTE
+disk_limit = (disk_used / 100 * 20) * BYTE_TO_MEGABYTE # 20% of the disk
 
 # this block of code is required because Microsoft's cmd doesn't use "clear"
 if platform.system() == 'Windows':

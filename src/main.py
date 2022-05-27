9c194cd609f0c66cdd3b21b7fa7db0265f53bdbb
Gibsol <gibsol75@gmail.com>
2022-05-27 16:44:06 +0300

    added working with history

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 import psutil
 import platform
 
@@ -18,21 +19,46 @@ disk_total = getattr(disk, 'free') * BYTE_TO_MEGABYTE
 disk_used = getattr(disk, 'used') 
 disk_limit = (disk_used / 100 * 20) * BYTE_TO_MEGABYTE # 20% of the disk
 
-# this block of code is required because Microsoft's cmd doesn't use "clear"
+# information about both
+def memory_info():
+    print('RAM:')
+    print('Your total memory: ', round(memory_total, 2), 'MB')
+    print('Memory available: ', round(memory_free, 2), 'MB')
+
+def disk_info():
+    print('\nDisk usage:')
+    print('Your total disk memory: ', round(disk_free, 2), 'MB')
+    print('Disk memory available: ', round(disk_total, 2), 'MB')
+
+# this block of code is required because Microsoft's cmd uses "cls" instead of "clear"
 if platform.system() == 'Windows':
     os.system('cls')
 else:
     os.system('clear')
 
-print('RAM:')
-print('Your total memory: ', round(memory_total, 2), 'MB')
-print('Memory available: ', round(memory_free, 2), 'MB')
-
-print('\nDisk usage:')
-print('Your total disk memory: ', round(disk_free, 2), 'MB')
-print('Disk memory available: ', round(disk_total, 2), 'MB')
+memory_info()
+disk_info()
 
 if memory_used > memory_limit:
     print('WARNING: You are using more than 20% of your RAM')
 elif disk_used > disk_limit:
     print('WARNING: You are using more than 20% of your disk space')
+
+# entering history of the progarm in the "history.txt"
+with open('history.txt', mode='a')as history:
+    current_time = str(datetime.now()) 
+
+    history.write(current_time)
+    history.write('\nRAM:\n')
+    history.write(str(round(memory_total, 2)))
+    history.write('\n')
+    history.write(str(round(memory_free, 2)))
+    history.write('\n')
+
+    history.write('\ndisk:\n')
+    history.write(str(round(disk_free, 2)))
+    history.write('\n')
+    history.write(str(round(disk_total, 2)))
+    history.write('\n\n')
+
+    history.close()
